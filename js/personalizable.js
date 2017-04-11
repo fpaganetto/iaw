b2136f241d474be796f26c4bd39e53e3af1c083d
nu11pointer <federico.paganetto@hotmail.com>
2017-04-11 18:54:52 -0300

    Agregados cambios al carousel y otros visuales

---
+++ js/personalizable.js
@@ -0,0 +1,7 @@
+/*La idea es retornar un arreglo multidimensional*/
+
+function getDesingElements(){
+	alert( "Entro a la funcion getDesingElements");
+	var color = ["Amarillo","Verde","Rojo","Azul","Cromado"];
+	return JSON.stringify(color);
+}
