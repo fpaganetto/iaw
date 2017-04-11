3c05f0ff3f36c7f07531ddd2f1d2b7a1342a281e
nu11pointer <federico.paganetto@hotmail.com>
2017-04-11 19:14:13 -0300

    Eliminados archivos inutiles

--- js/personalizable.js
+++
@@ -1,7 +0,0 @@
-/*La idea es retornar un arreglo multidimensional*/
-
-function getDesingElements(){
-	alert( "Entro a la funcion getDesingElements");
-	var color = ["Amarillo","Verde","Rojo","Azul","Cromado"];
-	return JSON.stringify(color);
-}
