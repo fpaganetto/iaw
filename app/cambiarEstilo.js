810cbeaa6e453870c44dacb38d3b59dde47daff5
nu11pointer <federico.paganetto@hotmail.com>
2017-04-13 15:41:56 -0300

    Cambios visuales ahora en Chrome para moviles la omnibar cambia de color los botones ahora son del color acorde al tema

--- app/cambiarEstilo.js
+++ app/cambiarEstilo.js
@@ -1,6 +1,8 @@
 $('#est1').click(function (){
    $('link[href="css/style2.css"]').attr('href','css/style1.css');
+   $('meta[name="theme-color"]').attr('content','#000000');
 });
 $('#est2').click(function (){
    $('link[href="css/style1.css"]').attr('href','css/style2.css');
+   $('meta[name="theme-color"]').attr('content','#ffffff');
 });
