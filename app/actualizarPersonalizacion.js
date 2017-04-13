2256d3ad7af3947517ebb1f8669d21b48e316a08
Necdilzor <pachecoagustin@hotmail.com>
2017-04-13 13:36:47 -0300

    opciones personalizables cargadas via Canvas

--- app/actualizarPersonalizacion.js
+++ app/actualizarPersonalizacion.js
@@ -1,6 +1,29 @@
 function opcionSeleccionada(opcion, valor) {
-  alert("Seleccionó: "+opcion+" -> "+valor);
+  console.log("Seleccionó: "+opcion+" -> "+valor);
 
+  //obtenemos la imagen original de existir
+/*  var imagenOpcionSeleccionada = document.getElementById("opcion-"+opcion);
+  var existeID = true;
+  if (imagenOpcionSeleccionada == null) {
+    existeID = false;
+    imagenOpcionSeleccionada = document.createElement("img");
+  }
+  imagenOpcionSeleccionada.setAttribute("src", "app/img/"+opcion+"/"+valor+".png");
+  imagenOpcionSeleccionada.setAttribute("class", "imagen-opcion");
+  imagenOpcionSeleccionada.setAttribute("id", "opcion-"+opcion);
+
+  if (!existeID) {
+    document.getElementById("imagen-personalizada").appendChild(imagenOpcionSeleccionada);
+  }*/
+
+
+  var canvas = document.getElementById("imagen-personalizada");
+  var ctx = canvas.getContext("2d");
+  var imageObj = new Image();
+  imageObj.onload = function() {//dibujar cuando la imagen esté cargada
+    ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+  imageObj.src = "app/img/"+opcion+"/"+valor+".png";
 }
 
 $( "p" ).click(function() { //ya comprobé que este funciona
