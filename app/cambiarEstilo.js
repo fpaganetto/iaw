5a5b17dc1991ab1d8d485eaa27b9a6aa85f0ec8c
nu11pointer <federico.paganetto@hotmail.com>
2017-04-13 15:08:14 -0300

    Agregado el cambio del estilo css No recuerda el estado

---
+++ app/cambiarEstilo.js
@@ -0,0 +1,6 @@
+$('#est1').click(function (){
+   $('link[href="css/style2.css"]').attr('href','css/style1.css');
+});
+$('#est2').click(function (){
+   $('link[href="css/style1.css"]').attr('href','css/style2.css');
+});
