1fd377a80e6866a8473e63a43230f7d4bd86f328
nu11pointer <federico.paganetto@hotmail.com>
2017-04-18 22:51:49 -0300

    ahora el estilo se guarda en localStorage

--- js/cambiarEstilo.js
+++ js/cambiarEstilo.js
@@ -1,32 +1,22 @@
-//Cargamos el estilo preseleccionado inmediatamente
-var estilo = getCookie("estilo");
-var indice = 1; //indice: 0-> bootstrap, 1->style.css (propio) VARIABLE GLOBAL
-console.log(estilo);
-if (estilo != "") { //si ya hay una opción elegida previamente
-  cambiarEstilo(indice, estilo);
+if (localStorage.getItem("estilo")!=null) { //si ya hay una opción elegida previamente
+  cambiarEstilo(localStorage.getItem("estilo"));
 }
 
-
-
 $('#est1').click(function (){
-  cambiarEstilo(indice, "style1");
-   //$('link[href="css/style2.css"]').attr('href','css/style1.css');
-   $('meta[name="theme-color"]').attr('content','#000000');
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("estilo", "style1");
+    cambiarEstilo("style1");
+  }
+    $('meta[name="theme-color"]').attr('content','#000000');
 });
 $('#est2').click(function (){
-    cambiarEstilo(indice, "style2");
-   //$('link[href="css/style1.css"]').attr('href','css/style2.css');
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("estilo", "style2");
+    cambiarEstilo("style2");
+    }
    $('meta[name="theme-color"]').attr('content','#ffffff');
 });
 
-
-
-function cambiarEstilo(indiceCSS, estilo) { //indiceCSS: 0-> bootstrap, 1->style.css (propio)
-    var estiloCSS = document.getElementsByTagName("link").item(indiceCSS);
-    var nuevo = document.createElement("link");
-    nuevo.setAttribute("rel", "stylesheet");
-    nuevo.setAttribute("type", "text/css");
-    nuevo.setAttribute("href", "css/"+estilo+".css");
-    document.getElementsByTagName("head").item(0).replaceChild(nuevo, estiloCSS);
-    setCookie("estilo", estilo, 30); //guardar estilo en una cookie, válido por 30 días
+function cambiarEstilo(estilo) {
+  $('link[href*="css/style').attr('href','css/'+estilo+'.css');
 }
