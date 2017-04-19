c414ddf2f51c1193b0b557841c4e30d08536145c
nu11pointer <federico.paganetto@hotmail.com>
2017-04-19 01:19:45 -0300

    eliminado archivo auxiliar

--- app/cargarRecordado.js
+++
@@ -1,9 +0,0 @@
-function cargarRecordado(){
-	var cookieAuto = JSON.parse(getCookie("autoPersonalizado"));
-	//Opcion: Color, Llantas, Polarizado, Motor...
-	for(var opcion in cookieAuto){
-    	opcionSeleccionada(opcion,cookieAuto[opcion]);
-		var boton = $(".btn-personalizar:contains("+cookieAuto[opcion]+")");
-		boton.addClass("active");
-	}
-}
